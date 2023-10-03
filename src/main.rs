5529c4abd2c448bc8d57da440e1b5de32ed816de
Mayengbam Sushil Kumar <mayengbam.kumar@quincus.com>
2023-10-03 08:40:05 +0530

    Added another solution of prob_1 using filter/sum()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod prob_1;
 
 fn main() {
-    println!("Hello, world!");
+}
+#[test]
+fn test_solution() {
+    assert_eq!(prob_1::solution(1000), 233168);
+    assert_eq!(prob_1::solution(10), 23);
+    assert_eq!(prob_1::solution(5), 3);
 }
--- src/prob_1.rs
+++ src/prob_1.rs
@@ -1,15 +1,12 @@
 // https://projecteuler.net/problem=1
-fn solution() -> i32 {
-    let mut sum = 0;
-    for num in 1..1000 {
-        if num % 3 == 0 || num % 5 == 0 {
-            sum += num;
-        }
-    }
-    sum
+pub fn solution(upto: i32) -> i32 {
+    // let mut sum = 0;
+    // for num in 1..1000 {
+    //     if num % 3 == 0 || num % 5 == 0 {
+    //         sum += num;
+    //     }
+    // }
+    // sum
+    (1..upto).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
 }
 
-#[test]
-fn test_solution() {
-    assert_eq!(solution(), 233168);
-}
